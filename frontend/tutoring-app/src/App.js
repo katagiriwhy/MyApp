90bcf2cc3392dac70a10d681b8d19591347c1e05
katagiriwhy <karuk.vova2005@mail.ru>
2025-08-30 13:12:49 +0300

    w

--- frontend/tutoring-app/src/App.js
+++ frontend/tutoring-app/src/App.js
@@ -3,17 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/auth/LoginForm';
 import Register from './pages/auth/RegisterForm';
+import HomePage from "./pages/general/HomePage";
 
-const Home = () => <div> Home Page</div>;
-
-const NotFound = () => <div> Game is over</div>
+const NotFound = () => <div> Page is not found</div>;
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="*" element={<NotFound />} />
